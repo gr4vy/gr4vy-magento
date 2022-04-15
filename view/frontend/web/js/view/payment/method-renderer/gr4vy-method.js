2172c085b93f58bfc5722099968a1aa671f40d65
Rowan Burgess <rowandburgess@gmail.com>
2022-04-15 10:33:31 +0000

    G4-43 add shipping fee as cart items

--- view/frontend/web/js/view/payment/method-renderer/gr4vy-method.js
+++ view/frontend/web/js/view/payment/method-renderer/gr4vy-method.js
@@ -130,10 +130,39 @@ define(
                         }
                     });
 
+                    // calculate shipping method
+                    var shipping_fee = this.calculateShippingFee();
+                    if (typeof shipping_fee.unitAmount !== "undefied") {
+                        cartItemsData.push(shipping_fee);
+                    }
+
+                    console.log(cartItemsData);
+
                     return cartItemsData;
                 }
 
                 return [];
+            },
+            calculateShippingFee: function () {
+                var selected_shipping_method = window.checkoutConfig.selectedShippingMethod;
+                if (false) {
+                    // TODO when multi shipping address is supported, need to calculate it
+                }
+                else {
+                    var fee = {};
+                    if (selected_shipping_method.amount) {
+                        fee = {
+                            name: selected_shipping_method.method_title,
+                            quantity: 1,
+                            unitAmount: selected_shipping_method.amount * 100,
+                            sku: selected_shipping_method.method_code,
+                            productUrl: '',
+                            productType: 'shipping_fee'
+                        }
+                    }
+                }
+
+                return fee;
             },
             /**
              * @returns {Object}

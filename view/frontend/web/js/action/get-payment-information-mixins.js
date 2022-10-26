2810d657eaeac021a3da290ea164e62a2a78c552
Rowan Burgess <rowandburgess@gmail.com>
2022-10-26 11:19:07 +0000

    save code - checkout edit events

--- view/frontend/web/js/action/get-payment-information-mixins.js
+++ view/frontend/web/js/action/get-payment-information-mixins.js
@@ -1,9 +1,12 @@
 /*jshint browser:true jquery:true*/
 define([
     'mage/utils/wrapper',
+    'mage/storage',
+    'Magento_Checkout/js/model/payment/renderer-list',
     'Gr4vy_Magento/js/model/config',
-    'Magento_Checkout/js/model/full-screen-loader'
-], function (wrapper, config, loader) {
+    'Magento_Checkout/js/model/full-screen-loader',
+    'jquery'
+], function (wrapper, storage, renderer, config, loader, $) {
     'use strict';
 
     /**
@@ -21,6 +24,38 @@ define([
             }
 
             loader.startLoader();
+
+            storage.post(config.reloadConfigUrl, {}, false, 'application/json')
+                .done(function (result) {
+                    var removeEntries = [];
+
+                    // Removing Gr4vy
+                    renderer.each(function (value, index) {
+                        if (value.type.startsWith('gr4vy')) {
+                            removeEntries.push(value);
+                        }
+                    });
+
+                    $.each(removeEntries, function (index, entry) {
+                        renderer.remove(entry);
+                    })
+
+                    // Adding new Gr4vy Payment
+                    $.each(result['payment'], function (index, entry) {
+                        if (index.startsWith('gr4vy')) {
+                            window.checkoutConfig.payment.gr4vy = entry;
+                            config.token(entry.token);
+                            config.amount(entry.total_amount);
+                            config.cartItems(entry.items);
+                            config.intent(entry.intent);
+
+                            renderer.push({
+                                type: index,
+                                component: 'Gr4vy_Magento/js/view/payment/method-renderer/gr4vy-method'
+                            });
+                        }
+                    });
+                });
 
             return originalGetPaymentInformation().then(function () {
                 loader.stopLoader();

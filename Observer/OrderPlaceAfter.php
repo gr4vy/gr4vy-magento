69b0005902cce359981adeddea3e24f9524044f3
ali <ali@gr4vy.com>
2024-06-26 15:47:09 +0100

    handling post order placement
    After the order is created by Magento, we are updating the order data including order status to reflect the tx status in Gr4vy.

--- Observer/OrderPlaceAfter.php
+++ Observer/OrderPlaceAfter.php
@@ -6,10 +6,12 @@ use Gr4vy\Magento\Model\Payment\Gr4vy;
 use Gr4vy\Magento\Helper\Data as Gr4vyHelper;
 use Gr4vy\Magento\Helper\Logger as Gr4vyLogger;
 use Gr4vy\Magento\Helper\Order as OrderHelper;
+use Gr4vy\Magento\Api\TransactionRepositoryInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
+use Magento\Quote\Api\CartRepositoryInterface;
 
 /**
  * Update order status after place order
@@ -17,10 +19,22 @@ use Magento\Sales\Model\Order;
 class OrderPlaceAfter implements ObserverInterface
 {
     const NEW_ORDER_STATUS = Order::STATE_PENDING_PAYMENT;
+    const ORDER_STATUS_PENDING = 'pending';
+
     /**
      * @var OrderRepositoryInterface
      */
     private $orderRepository;
+
+    /**
+     * @var CartRepositoryInterface
+     */
+    private $cartRepository;
+
+    /**
+     * @var TransactionRepositoryInterface
+     */
+    protected $transactionRepository;
 
     /**
      * @var OrderHelper
@@ -40,17 +54,23 @@ class OrderPlaceAfter implements ObserverInterface
     /**
      * OrderPlaceAfter constructor.
      * @param OrderRepositoryInterface $orderRepository
+     * @param CartRepositoryInterface $cartRepository
+     * @param TransactionRepositoryInterface $transactionRepository
      * @param OrderHelper $orderHelper
      * @param Gr4vyLogger $gr4vyLogger
      * @param Gr4vyHelper $gr4vyHelper
      */
     public function __construct(
         OrderRepositoryInterface    $orderRepository,
+        CartRepositoryInterface $cartRepository,
+        TransactionRepositoryInterface $transactionRepository,
         OrderHelper $orderHelper,
         Gr4vyLogger $gr4vyLogger,
         Gr4vyHelper $gr4vyHelper
     ) {
         $this->orderRepository = $orderRepository;
+        $this->cartRepository = $cartRepository;
+        $this->transactionRepository = $transactionRepository;
         $this->orderHelper = $orderHelper;
         $this->gr4vyLogger = $gr4vyLogger;
         $this->gr4vyHelper = $gr4vyHelper;
@@ -65,6 +85,7 @@ class OrderPlaceAfter implements ObserverInterface
     {
         $order = $observer->getEvent()->getData('order');
         $payment = $order->getPayment();
+        $quote = $observer->getEvent()->getQuote();
 
         if ($payment->getMethod() != Gr4vy::PAYMENT_METHOD_CODE) {
             $this->gr4vyLogger->logMixed(['Processing Non Gr4vy Order']);
@@ -73,18 +94,67 @@ class OrderPlaceAfter implements ObserverInterface
     
         if ($this->gr4vyHelper->checkGr4vyReady()) {
             try {
-                $order->setState(self::NEW_ORDER_STATUS)->setStatus(self::NEW_ORDER_STATUS);
-                $order->setIsCustomerNotified(false);
+                $gr4vy_transaction_id = $payment->getData('gr4vy_transaction_id');
+                $transaction = $this->transactionRepository->getByGr4vyTransactionId($gr4vy_transaction_id);
+
+                // only applicable for gr4vy payment method
+                if ($payment->getMethod() != Gr4vy::PAYMENT_METHOD_CODE
+                    || strlen($gr4vy_transaction_id) < 1)
+                {
+                    return false;
+                }
+
+                $orderStatus = self::ORDER_STATUS_PENDING;
+                $msg = __("");
+
+                if ($this->gr4vyHelper->getGr4vyIntent() === Gr4vy::PAYMENT_TYPE_AUCAP) {
+                    $orderStatus = Order::STATE_PROCESSING;
+                    if (!$order->canInvoice()) {
+                        $msg = __("Error in creating an Invoice.");
+                    } else {
+                        $msg = __(
+                            "Captured amount of %1 online. Transaction ID: '%2'.",
+                            $this->gr4vyHelper->formatCurrency($transaction->getCapturedAmount()/100),
+                            strval($transaction->getGr4vyTransactionId())
+                        );
+                        $this->orderHelper->generatePaidInvoice($order, $gr4vy_transaction_id);
+                    }
+                } else if ($this->gr4vyHelper->getGr4vyIntent() === Gr4vy::PAYMENT_TYPE_AUTH) {
+                    $msg = __(
+                        "Authorized amount of %1 online. Transaction ID: '%2'.",
+                        $this->gr4vyHelper->formatCurrency($transaction->getAmount()/100),
+                        strval($transaction->getGr4vyTransactionId())
+                    );
+                }
+
+                $this->orderHelper->updateOrderStatus($order, $orderStatus);
                 $this->orderHelper->updateOrderHistoryData(
                     $order->getEntityId(),
-                    self::NEW_ORDER_STATUS,
-                    __('Order has been placed by Magento.')
+                    $orderStatus,
+                    $msg
                 );
+
                 $this->orderRepository->save($order);
+                $this->disableQuote($quote);
+                return true;        
             }
             catch (\Exception $e) {
                 $this->gr4vyLogger->logException($e);
             }
         }
+    }
+
+    /**
+     * Disable a quote after successful payment
+     *
+     * @param Quote $quote
+     */
+    public function disableQuote($quote)
+    {
+        if (!$quote || !$quote->getIsActive()) {
+            return;
+        }
+        $quote->setIsActive(false);
+        $this->cartRepository->save($quote);
     }
 }

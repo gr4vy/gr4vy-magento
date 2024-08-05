27015d71cec6da1306e923a844285a2520072655
ali <ali@gr4vy.com>
2024-08-05 09:46:11 +0100

    adding CSP support

--- Block/Head/Embed.php
+++ Block/Head/Embed.php
@@ -5,23 +5,36 @@ declare(strict_types=1);
 namespace Gr4vy\Magento\Block\Head;
 
 use Gr4vy\Magento\Helper\Data as Gr4vyHelper;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+use Gr4vy\Magento\Helper\Logger as Gr4vyLogger;
 
 class Embed extends \Magento\Framework\View\Element\AbstractBlock
 {
-    const EMBED_TEMPLATE = '<script>require.config({ config: { mixins: { \'Magento_Checkout/js/sidebar\': { \'Gr4vy_Magento/js/sidebar-mixins\': true } } }, map: { \'*\': { gr4vyapi: \'https://cdn.{gr4vyId}.gr4vy.app/embed.latest.js\' } } });</script>';
+    const EMBED_TEMPLATE = 'require.config({ config: { mixins: { \'Magento_Checkout/js/sidebar\': { \'Gr4vy_Magento/js/sidebar-mixins\': true } } }, map: { \'*\': { gr4vyapi: \'https://cdn.{gr4vyId}.gr4vy.app/embed.latest.js\' } } });';
 
     /**
      * @var Gr4vyHelper
      */
     protected $gr4vy_helper;
 
+    /**
+     * @var Gr4vyLogger
+     */
+    private $gr4vyLogger;
+
+    private $secureRenderer;
+
     public function __construct(
         \Magento\Framework\View\Element\Context $context,
         Gr4vyHelper $gr4vy_helper,
+        Gr4vyLogger $gr4vyLogger,
+        SecureHtmlRenderer $secureRenderer,
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->secureRenderer = $secureRenderer;
         $this->gr4vy_helper = $gr4vy_helper;
+        $this->gr4vyLogger = $gr4vyLogger;
     }
 
     /**
@@ -31,7 +44,9 @@ class Embed extends \Magento\Framework\View\Element\AbstractBlock
      */
     protected function _toHtml() {
         $gr4vy_id = $this->gr4vy_helper->getGr4vyId();
-        return $this->renderLinkTemplate($gr4vy_id);
+        $script = $this->renderLinkTemplate($gr4vy_id);
+        $secureTag = $this->secureRenderer->renderTag('script', ['type' => 'text/javascript'], $script, false);
+        return $secureTag;
     }
 
     /**
